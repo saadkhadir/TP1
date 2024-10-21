824eb47483220889a8dc1c7ee6d6af35c87277dc
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2024-10-21 22:55:38 +0100

    first commit

--- src/CptOcc.java
+++ src/CptOcc.java
@@ -6,10 +6,24 @@ public class CptOcc {
         System.out.println("Veuillez entrer une chaine de caractère");
         String chaine = scanner.nextLine().toLowerCase();
 
-        int[] nb_occurences= new int[26];
+        int[] nb_occurrences= new int[26];
 
-        for (char alphabet=0;alphab)
+        for (char ch='a'; ch<='z';ch++){
+            int occ=0;
+            for(int i=0;i<chaine.length();i++){
+                if(chaine.charAt(i)==ch){
+                   occ++;
+                }
+            }
+          nb_occurrences[ch - 'a'] = occ;
 
-
+        }
+        System.out.println("la chaine" + chaine + "contient:");
+        for(int i=0;i<nb_occurrences.length;i++){
+            if(nb_occurrences[i] > 0){
+                char lettre = (char) (i + 'a');
+                System.out.println(nb_occurrences[i] + " fois la lettre '" + Character.toUpperCase(lettre) + "'");
+            }
+        }
     }
 }
